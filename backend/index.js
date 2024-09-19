e366050a9ab5f82496b9bb15dfa8f3144ed23af3
Toe Arkar <toearkar.15@gmail.com>
2024-09-18 22:48:29 -0400

    CI CD caching

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 // route
 app.use("/v1/api/product", productRoute);
 
-app.get("/", (req, res) => res.send("CI/CD Test 1"));
+app.get("/", (req, res) => res.send("CI/CD Test (2)"));
 
 mongoose
   .connect(dbUrl, {
