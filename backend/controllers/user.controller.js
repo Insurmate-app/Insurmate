3561eda1bc2479109ede36692954724b8e82adca
Toe Arkar <toearkar.15@gmail.com>
2024-10-29 11:37:13 -0400

    Fixed Error

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,4 +1,8 @@
 const userService = require("../services/user.service");
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = process.env.JWT_SECRET;
+const passport = require("passport");
+require("../security/passport"); // Import Passport JWT configuration
 
 const createUser = async (req, res, next) => {
   try {
@@ -24,7 +28,19 @@ const loginUser = async (req, res, next) => {
   try {
     const { email, password } = req.body;
     await userService.loginUser(email, password);
-    res.status(200).json("Login successful.");
+
+    // Generate JWT token
+    const token = jwt.sign({ id: email }, JWT_SECRET, {
+      expiresIn: "15m", // 15 minutes
+    });
+    // Set the token as a cookie (HttpOnly for security)
+    res.cookie("token", token, {
+      httpOnly: true, // Makes the cookie inaccessible to JavaScript on the client-side
+      secure: process.env.NODE_ENV === "production", // Ensure HTTPS in production
+      maxAge: 900000, // 1 hour in milliseconds
+    });
+
+    res.status(200).json({ message: token });
   } catch (error) {
     // Pass the error to the global error handler using next()
     //res.status(500).json({ message: error.message });
@@ -52,10 +68,25 @@ const regenerateOtp = async (req, res, next) => {
   }
 };
 
+const logoutUser = (req, res, next) => {
+  try {
+    // Clear the cookie by setting the token cookie's maxAge to
+    res.clearCookie("token", passport.authenticate("jwt", { session: false }), {
+      httpOnly: true, // Ensure cookie is only accessible by the server
+      secure: process.env.NODE_ENV === "production", // Only use HTTPS in production
+    });
+    // Send a response indicating the user has been logged out
+    res.status(200).json({ message: "Logged out successfully." });
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports = {
   createUser,
   resetPassword,
   loginUser,
   verifyUser,
   regenerateOtp,
+  logoutUser,
 };
